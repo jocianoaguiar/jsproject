233f6639d8aa083141744c20096ffdb09a69c19d
jocianoaguiar <123665642+jocianoaguiar@users.noreply.github.com>
2023-03-30 15:46:13 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,18 @@
+$('.menumobile').click(function (){
+    $('.menumobile').css('width','100%')
+    $('.menumobile').css('height','auto')
+    $('.menumobile').css('overflow','auto')
+    $('.menumobile>ul').css('display','inline-block')
+    $('.menumobile').css('background-image','url(../imagens/bg.webp)')
+})
+
+  function segundoclick() {
+  $('.menumobile').click(function (){
+  $('.menumobile').css('width','60px%')
+  $('.menumobile').css('height','60px')
+  $('.menumobile').css('overflow','hidden')
+  $('.menumobile>ul').css('display','none')
+  $('.menumobile').css('background-image','url(../imagens/bg.webp)')
+})
+  }
+  
